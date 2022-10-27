476dc88c5298a33eab5ff681a53033d303bbf4be
jorge <geolg763@gmail.com>
2022-10-27 08:45:25 -0500

    create a simple webpage

---
+++ scripts/contracts/metasender.js
@@ -0,0 +1,11 @@
+const metasender = {
+	name: "Metasender",
+	//change
+	//mainet contract
+	address: "",
+	//goeli contract
+	//address: "0x50c937BB33936BcA0454e128A82F71eb918A0fF1",
+	abi: [],
+};
+
+export default metasender;
