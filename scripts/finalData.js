826f424fea5283f26c58a5c0bfe6c301cde586a9
Carlos Mario <carlosmario7lg@gmail.com>
2022-10-31 15:22:33 -0500

    finish process data

---
+++ scripts/finalData.js
@@ -0,0 +1,42 @@
+import { walletsFileArr, amountFileArr, tokenToSendFile } from "./readFile.js";
+import {
+  walletsManualArr,
+  amountManualArr,
+  tokenToSendManual,
+} from "./manualWallets.js";
+const finalData = {
+  tokenToSend: "",
+  wallets: [],
+  amount: [],
+};
+const btnProcessManual = document.querySelector(".process-btn-manual");
+const btnProcessFile = document.querySelector(".process-btn-file");
+
+btnProcessManual.addEventListener("click", () => {
+  processFinalData();
+});
+
+btnProcessFile.addEventListener("click", () => {
+  processFinalData();
+});
+
+function processFinalData() {
+  if (walletsFileArr.length != 0 && amountFileArr.length != 0) {
+    setFinalData(walletsFileArr, amountFileArr, tokenToSendFile);
+  } else if (walletsManualArr.length != 0 && amountManualArr.length != 0) {
+    setFinalData(walletsManualArr, amountManualArr, tokenToSendManual);
+  }
+}
+
+function setFinalData(walletsArr, amountArr, tokenToSend) {
+  //console.log(typeof amountArr[0]);
+  finalData.tokenToSend = tokenToSend;
+  finalData.wallets = [...walletsArr];
+  let amountInBigNumber = amountArr.map((amount) =>
+    ethers.utils.parseEther(amount.toString())
+  );
+  finalData.amount = amountInBigNumber;
+  console.log(finalData);
+}
+
+export { finalData };
