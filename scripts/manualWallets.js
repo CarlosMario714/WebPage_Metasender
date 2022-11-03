e7faae4c1bd36c773f0aee6c8fb9b7b4fbf62798
Carlos Mario <carlosmario7lg@gmail.com>
2022-11-03 17:20:10 -0500

    add regular expresion validations

--- scripts/manualWallets.js
+++ scripts/manualWallets.js
@@ -13,6 +13,8 @@ const sendProcessSection = document.getElementById("send-process");
 const tokenInput = document.getElementById("token-input");
 const labelAdress = document.querySelector(".label-adress");
 const labelAmount = document.querySelector(".label-amount");
+const spanWallet = document.querySelector(".span-wallet");
+const spanAmount = document.querySelector(".span-amount");
 
 let walletsManualArr = [];
 let amountManualArr = [];
@@ -21,24 +23,31 @@ let tokenToSendManual = tokenInput.value;
 let numberOfWallets = 0;
 let domElementWallet;
 
+//expresion regular numeros positivos enteros ^\d*\d+$
+//expresion regular numeros positivos enteros y con decimales ^\d*\.\d+$|^\d*\d+$
 tokenInput.addEventListener("click", (e) => {
   switch (tokenInput.value) {
     case "ETH":
       labelAdress.innerHTML = "Cuenta o Wallet a enviar";
       walletInput.placeholder = "Escribe la wallet";
       labelAmount.innerHTML = "Monto a Enviar";
-      labelAmount.placeholder = "Escribe el monto";
+      amountInput.placeholder = "Escribe el monto";
+      amountInput.pattern = `^\\d*\\.\\d+$|^\\d*\\d+$`;
       break;
     case "ERC20":
       labelAdress.innerHTML = "Adress del contrato del token";
       walletInput.placeholder = "Escribe el Adress del contrato";
-      labelAmount.innerHTML = "# de tokens a enviar";
-      labelAmount.placeholder = "Escribe el # de tokens";
+      labelAmount.innerHTML = "Cantidad de tokens a enviar";
+      amountInput.placeholder = "Escribe cantidad de tokens";
+      amountInput.pattern = `^\\d*\\.\\d+$|^\\d*\\d+$`;
+      break;
     case "ERC721":
       labelAdress.innerHTML = "Adress del contrato del token";
       walletInput.placeholder = "Escribe el Adress del contrato";
-      labelAmount.innerHTML = "# de tokens a enviar";
-      labelAmount.placeholder = "Escribe el # de tokens";
+      labelAmount.innerHTML = "ID del token a enviar";
+      amountInput.placeholder = "Escribe el ID del token";
+      amountInput.pattern = `^\\d*\\d+$`;
+      break;
   }
 });
 
@@ -47,9 +56,32 @@ optionManual.addEventListener("click", () => {
   fileDataContainer.style.display = "none";
 });
 
-addWalletButton.addEventListener("click", () => {
-  addNewManualWallet();
-  continueBtnManual.classList.add("opacity");
+addWalletButton.addEventListener("click", (e) => {
+  if (walletInput.value && amountInput.value !== "") {
+    let walletRegex = new RegExp(walletInput.pattern);
+    let AmountRegex = new RegExp(amountInput.pattern);
+
+    !walletRegex.exec(walletInput.value)
+      ? spanWallet.classList.add("is-active")
+      : spanWallet.classList.remove("is-active");
+
+    !AmountRegex.exec(amountInput.value)
+      ? spanAmount.classList.add("is-active")
+      : spanAmount.classList.remove("is-active");
+
+    // console.log(
+    //   walletRegex.exec(walletInput.value) && AmountRegex.exec(amountInput.value)
+    // );
+
+    if (
+      walletRegex.exec(walletInput.value) &&
+      AmountRegex.exec(amountInput.value)
+    ) {
+      addNewManualWallet();
+      continueBtnManual.classList.add("opacity");
+      console.log(amountInput.pattern);
+    }
+  }
 });
 
 manualWalletsContainer.addEventListener("click", (e) => {
@@ -65,15 +97,17 @@ function addNewManualWallet() {
     `wallet-${numberOfWallets}-adress`,
     "manual-wallets"
   );
-  newWalletContainer.innerHTML = `<div class="manual-wallet">
-  <div>${numberOfWallets}</div>
-  <div class="wallet-1-adress">${walletInput.value}</div>
-  <div class="wallet-1-amount">${amountInput.value} ETH</div>
-  <a>
-    <img class="delete-wallet" data-wallet="${numberOfWallets}" src="../img/icons/close.svg"
-      alt="cerrar pagina" />
-  </a>
-</div>`;
+  newWalletContainer.innerHTML = `
+  <div class="manual-wallet">
+    <div>${numberOfWallets}</div>
+    <div>${walletInput.value}</div>
+    <div>${amountInput.value}</div>
+    <div>${tokenInput.value}</div>
+    <a>
+      <img class="delete-wallet" data-wallet="${numberOfWallets}" src="../img/icons/close.svg"
+        alt="cerrar pagina" />
+    </a>
+  </div>`;
   manualWalletsContainer.append(newWalletContainer);
   walletsManualArr.push(walletInput.value);
   amountManualArr.push(amountInput.value);
