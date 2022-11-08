0f9dcb6d5144b80de5df6f1a8b296a1b719bf4c7
Carlos Mario <carlosmario7lg@gmail.com>
2022-11-08 18:01:16 -0500

    start processing file function

--- scripts/readFile.js
+++ scripts/readFile.js
@@ -2,20 +2,18 @@ const dropArea = document.querySelector(".drop-area");
 const dragText = dropArea.querySelector("h2");
 const button = dropArea.querySelector("button");
 const input = dropArea.querySelector("#input-file");
-const body = document.querySelector("body");
 const fileDataContainer = document.querySelector(".file-data-container");
 const manualDataContainer = document.querySelector(".manual-data-container");
 const optionFile = document.querySelector(".option-file");
-const optionManual = document.querySelector(".option-manual");
-const sendProcessSection = document.getElementById("send-process");
 const child1DropArea = document.querySelector(".drop-area h2");
 const child2DropArea = document.querySelector(".drop-area span");
 const tokenInput = document.getElementById("token-input");
+const continueBtnFile = document.querySelector(".continue-btn-file");
 
 let tokenToSendFile = tokenInput.value;
 let walletsFileArr = [];
 let amountFileArr = [];
-let files;
+let file;
 
 button.addEventListener("click", () => {
   input.click();
@@ -29,12 +27,11 @@ optionFile.addEventListener("click", () => {
 //detecta cada vez que input cambia, osea cada vez que se sube un archivo
 input.addEventListener("change", (e) => {
   e.preventDefault();
-  files = e.target.files[0];
-  console.log(files);
+  file = e.target.files[0];
 
-  processFile(files);
+  showFile(file);
+
   dropArea.classList.add("active");
-  //showFiles(files);
   dropArea.classList.remove("active");
 });
 
@@ -55,21 +52,15 @@ dropArea.addEventListener("dragleave", (e) => {
 //cuando de suelta un elemento en el area
 dropArea.addEventListener("drop", (e) => {
   e.preventDefault();
-  files = e.dataTransfer.files[0];
+  file = e.dataTransfer.files[0];
 
-  processFile(files);
+  showFile(file);
 
-  //showFiles(files);
   dropArea.classList.remove("active");
   dragText.textContent = "arrastra y suelta el archivo ";
 });
 
-async function processFile(file) {
-  const data = await file.arrayBuffer();
-  const workbook = XLSX.readFile(data);
-  const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-  const excelData = XLSX.utils.sheet_to_json(worksheet);
-
+function showFile(file) {
   const previewElement = document.createElement("div");
   previewElement.classList.add("file-container");
   previewElement.innerHTML = `<img src="../img/icons/excel-icon.png" alt="${file.name}" width="50">
@@ -83,6 +74,15 @@ async function processFile(file) {
   dropArea.removeChild(child2DropArea);
   dropArea.removeChild(button);
   dropArea.append(previewElement);
+}
+
+continueBtnFile.addEventListener("click", processFile);
+
+async function processFile() {
+  const data = await file.arrayBuffer();
+  const workbook = XLSX.readFile(data);
+  const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+  const excelData = XLSX.utils.sheet_to_json(worksheet);
 
   for (const adress of excelData) {
     walletsFileArr.push(adress.address);
