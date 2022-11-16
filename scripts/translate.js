a90851ab5e0760bab13de6764eb73b0c5f6514c6
JorgeLpzGnz <geolg763@gmail.com>
2022-11-16 10:58:25 -0500

    init texts transalations

---
+++ scripts/translate.js
@@ -0,0 +1,56 @@
+const languageButtons = document.querySelector(".language");
+let languaje = "es";
+
+function translateText( items, property, texts ) {
+
+  for (const textToTranslate of items) {
+
+    const section = textToTranslate.dataset.content;
+
+    const type = textToTranslate.dataset.type;
+
+    textToTranslate[ property ] = texts[section][type];
+    
+  }
+
+}
+
+// change Language
+
+async function changeLanguage(element) {
+
+  const textsToTranslate = document.querySelectorAll("[data-content]");
+
+  const requestJson = await fetch(`../json/${element}.json`);
+
+  const texts = await requestJson.json();
+
+  translateText( textsToTranslate, 'innerHTML', texts )
+
+  //traduccion formulario
+//   inputName.placeholder = texts["form"]["input-name-placeholder"];
+//   inputName.title = texts["form"]["input-name-title"];
+//   inputEmail.placeholder = texts["form"]["input-email-placeholder"];
+//   inputEmail.title = texts["form"]["input-email-title"];
+//   inputWallet.placeholder = texts["form"]["input-wallet-placeholder"];
+//   inputWallet.title = texts["form"]["input-wallet-title"];
+//   inputSubmit.value = texts["form"]["input-submit-value"];
+}
+
+function toggleLanguage(e) {
+
+  if (e.target.dataset.function == "en") {
+    changeLanguage(e.target.dataset.function);
+    languaje = "en";
+  }
+
+  if (e.target.dataset.function == "es") {
+    changeLanguage(e.target.dataset.function);
+    languaje = "es";
+  }
+
+}
+
+languageButtons.addEventListener("click", toggleLanguage);
+
+export { languaje };
