0d7dac8cf9e5c30b74bad866179adf9e9ef70625
JorgeLpzGnz <geolg763@gmail.com>
2022-11-18 17:49:14 -0500

    comment mainets

--- scripts/contracts/metasender.js
+++ scripts/contracts/metasender.js
@@ -2,19 +2,19 @@ const metasender = {
 	name: "Metasender",
 	//change
 	// Ethereum Networks
-	address_0x1: "",
+	// address_0x1: "",
 	address_0x5: "0x25604BB91DB2C95302cb0746eFeC239f394F252f",
 	// binance Networks
-	address_0x56: "",
+	// address_0x56: "",
 	address_0x61: "0xDD979b3a607c6255e124B80E7C0cF1d5F2649Bd5",
 	// polygon Networks
-	address_0x89: "",
+	// address_0x89: "",
 	address_0x13881: "0x8429B1975a180cbD4AbeefD27e0B669dD2179c00",
 	// Avalanche Networks
-	address_0xa86a: "",
+	// address_0xa86a: "",
 	address_0xa869: "0x8429B1975a180cbD4AbeefD27e0B669dD2179c00",
 	// fantom Networks
-	address_0xfa: "",
+	// address_0xfa: "",
 	address_0xafa2: "0x4Ce6C8658e7cB676c24B212e0e60f9aa913f4ec4",
 	abi: [
 		"function PALCO( address _address ) public view returns( bool )",
